2b63505d0be2ca278607890c6c0f81fc784566f0
Mitchell <mitch.ohara-wild@monash.edu>
2018-06-02 13:50:42 +1000

    Fixed tsibblestats package documentation

--- R/tsibblestats.R
+++ R/tsibblestats.R
@@ -1,17 +1,3 @@
-#' Forecasting Functions for Tidy Time Series
-#'
-#' Methods and tools for displaying and analysing univariate time series
-#' forecasts in a tidy format including exponential smoothing via state space models and
-#' automatic ARIMA modelling.
-#'
-#' \tabular{ll}{ Package: \tab tidyforecast\cr Type: \tab Package\cr License: \tab
-#' GPL3\cr LazyLoad: \tab yes\cr }
-#'
-#' @docType package
-#' @name tidyforecast-package
-#' @author Rob J Hyndman
-#'
-#' Maintainer: Rob.Hyndman@monash.edu
 #' @keywords package
 "_PACKAGE"
 
