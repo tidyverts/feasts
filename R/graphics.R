6b4e75cb169f852ea7b208102fe5fccd33abe1cb
Mitchell <mitch.ohara-wild@monash.edu>
2019-02-11 17:45:45 +1100

    Added graphics functions for ggseasonplot, ggsubseriesplot & gglagplot

---
+++ R/graphics.R
@@ -0,0 +1,176 @@
+#' @inherit forecast::ggseasonplot
+#'
+#' @examples
+#' tsibbledata::ausretail %>%
+#'   filter(
+#'     State == "Victoria",
+#'     Industry == "Cafes, restaurants and catering services"
+#'   ) %>%
+#'   ggseasonplot(Turnover)
+#'
+#' @rdname ggseasonplot
+#' @export
+ggseasonplot <- function(x, ...){
+  UseMethod("ggseasonplot")
+}
+
+#' @rdname ggseasonplot
+#' @importFrom ggplot2 ggplot aes geom_line
+#' @export
+ggseasonplot.tbl_ts <- function(x, var = NULL, period = "largest",
+                                facet_period = NULL, ...){
+  if(quo_is_null(enquo(var))){
+    inform(sprintf(
+      "Plot variable not specified, automatically selected `var = %s`",
+      measured_vars(x)[1]
+    ))
+    var <- sym(measured_vars(x)[1])
+  }
+  else{
+    var <- enexpr(var)
+  }
+
+  check_gaps(x)
+  idx <- index(x)
+
+  period <- get_frequencies(period, x)
+  if(period <= 1){
+    abort("The data must contain at least one observation per seasonal period.")
+  }
+  period <- period*time_unit(interval(x))
+
+  if(!is.null(facet_period)){
+    facet_period <- get_frequencies(facet_period, x)
+    if(facet_period <= 1){
+      abort("The data must contain at least one observation per seasonal period.")
+    }
+    facet_period <- facet_period*time_unit(interval(x))
+  }
+
+  x <- as_tibble(x) %>%
+    mutate(
+      id = units_since(!!idx) %/% period,
+      facet_id = units_since(!!idx) %/% facet_period %empty% NA,
+      !!as_string(idx) := !!idx - period * (units_since(!!idx) %/% period)
+    )
+
+  p <- ggplot(x, aes(x = !!idx, y = !!var, group = factor(!!sym("id")))) +
+    geom_line()
+
+  if(!is.null(facet_period)){
+    p <- p + facet_wrap(~ facet_id)
+  }
+
+  p
+}
+
+#' @inherit forecast::ggsubseriesplot
+#'
+#' @examples
+#' tsibbledata::ausretail %>%
+#'   filter(
+#'     State == "Victoria",
+#'     Industry == "Cafes, restaurants and catering services"
+#'   ) %>%
+#'   ggsubseriesplot(Turnover)
+#'
+#' @rdname ggsubseriesplot
+#' @export
+ggsubseriesplot <- function(x, ...){
+  UseMethod("ggsubseriesplot")
+}
+
+#' @rdname ggsubseriesplot
+#' @importFrom ggplot2 ggplot aes geom_line geom_hline facet_grid
+#' @export
+ggsubseriesplot.tbl_ts <- function(x, var = NULL, period = "smallest", ...){
+  if(quo_is_null(enquo(var))){
+    inform(sprintf(
+      "Plot variable not specified, automatically selected `var = %s`",
+      measured_vars(x)[1]
+    ))
+    var <- sym(measured_vars(x)[1])
+  }
+  else{
+    var <- enexpr(var)
+  }
+
+  check_gaps(x)
+  idx <- index(x)
+
+  period <- get_frequencies(period, x)
+  if(period <= 1){
+    abort("The data must contain at least one observation per seasonal period.")
+  }
+  period_units <- period*time_unit(interval(x))
+
+  x <- as_tibble(x) %>%
+    mutate(
+      id = !!idx - period_units*(units_since(!!idx)%/%period_units)
+    ) %>%
+    group_by(id) %>%
+    mutate(.yint = mean(!!var))
+
+  ggplot(x, aes(x = !!idx, y = !!var)) +
+    geom_line() +
+    facet_grid(~ id) +
+    geom_hline(aes(yintercept = .yint), colour = "blue")
+}
+
+
+#' @inherit forecast::gglagplot
+#'
+#' @examples
+#' tsibbledata::ausretail %>%
+#'   filter(
+#'     State == "Victoria",
+#'     Industry == "Cafes, restaurants and catering services"
+#'   ) %>%
+#'   gglagplot(Turnover)
+#'
+#' @rdname gglagplot
+#' @export
+gglagplot <- function(x, ...){
+  UseMethod("gglagplot")
+}
+
+#' @rdname gglagplot
+#' @importFrom ggplot2 ggplot aes geom_path facet_wrap
+#' @export
+gglagplot.tbl_ts <- function(x, var = NULL, period = "smallest", lags = 1:16, ...){
+  if(quo_is_null(enquo(var))){
+    inform(sprintf(
+      "Plot variable not specified, automatically selected `var = %s`",
+      measured_vars(x)[1]
+    ))
+    var <- sym(measured_vars(x)[1])
+  }
+  else{
+    var <- enexpr(var)
+  }
+
+  period <- get_frequencies(period, x)
+  if(period <= 1){
+    abort("The data must contain at least one observation per seasonal period.")
+  }
+  period_units <- period*time_unit(interval(x))
+
+  lag_exprs <- map(lags, function(lag) expr(lag(!!var, !!lag))) %>%
+    set_names(paste0(".lag_", lags))
+
+  idx <- index(x)
+
+  x <- x %>%
+    as_tibble %>%
+    mutate(
+      season = factor(!!idx - period_units*(units_since(!!idx)%/%period_units)),
+      !!!lag_exprs) %>%
+    gather(.lag, .value, !!names(lag_exprs)) %>%
+    mutate(.lag = factor(.lag, levels = names(lag_exprs), labels = paste("lag", lags)))
+
+  x %>%
+    ggplot(aes(x = !!var, y = .value, colour = season)) +
+    geom_abline(colour = "gray", linetype = "dashed") +
+    geom_path() +
+    facet_wrap(~ .lag)
+}
--- R/utils.R
+++ R/utils.R
@@ -13,3 +13,22 @@ require_package <- function(pkg){
     )
   }
 }
+
+check_gaps <- function(x){
+  if (any(has_gaps(x)$.gaps)) {
+    abort(sprintf("%s contains implicit gaps in time. You should check your data and convert implicit gaps into explicit missing values using `tsibble::fill_na()` if required.", deparse(substitute(x))))
+  }
+}
+
+`%empty%` <- function(x, y) {
+  if (length(x) == 0) y else x
+}
+
+lag <- function(x, n){
+  if (n == 0)
+    return(x)
+  xlen <- length(x)
+  n <- pmin(n, xlen)
+  out <- c(rep(NA, n), x[seq_len(xlen - n)])
+  out
+}
