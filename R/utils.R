7533d780ce99f574135eb1da5a45d67a09dad9e4
Mitchell <mitch.ohara-wild@monash.edu>
2019-02-12 22:51:31 +1100

    Updated function recommendation for filling gaps

--- R/utils.R
+++ R/utils.R
@@ -16,7 +16,7 @@ require_package <- function(pkg){
 
 check_gaps <- function(x){
   if (any(has_gaps(x)$.gaps)) {
-    abort(sprintf("%s contains implicit gaps in time. You should check your data and convert implicit gaps into explicit missing values using `tsibble::fill_na()` if required.", deparse(substitute(x))))
+    abort(sprintf("%s contains implicit gaps in time. You should check your data and convert implicit gaps into explicit missing values using `tsibble::fill_gaps()` if required.", deparse(substitute(x))))
   }
 }
 
