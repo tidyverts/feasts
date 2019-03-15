2997e2f9eca0b2ed9a35ffa14cd787209a6e3350
Mitchell <mitch.ohara-wild@monash.edu>
2019-03-15 12:40:18 +1100

    Added guerrero method

---
+++ R/guerrero.R
@@ -0,0 +1,34 @@
+# This R script contains code for extracting the Box-Cox
+# parameter, lambda, using Guerrero's method (1993).
+# Written by Leanne Chhay
+# Adapted for usage with features() by Mitchell O'Hara-Wild
+
+# guer.cv computes the coefficient of variation
+# Input:
+# 	lam = lambda
+# 	x = original time series as a time series object
+# Output: coefficient of variation
+
+guer.cv <- function(lam, x, .period = 2) {
+  nobsf <- length(x)
+  nyr <- floor(nobsf / .period)
+  nobst <- floor(nyr * .period)
+  x.mat <- matrix(x[(nobsf - nobst + 1):nobsf], .period, nyr)
+  x.mean <- apply(x.mat, 2, mean, na.rm = TRUE)
+  x.sd <- apply(x.mat, 2, sd, na.rm = TRUE)
+  x.rat <- x.sd / x.mean ^ (1 - lam)
+  res <- sd(x.rat, na.rm = TRUE) / mean(x.rat, na.rm = TRUE)
+  res
+}
+
+# guerrero extracts the required lambda
+# Input: x = original time series as a time series object
+# Output: lambda that minimises the coefficient of variation
+
+guerrero <- function(x, lower=-1, upper=2, .period) {
+  lambda <- optimize(
+    guer.cv, c(lower, upper), x = x,
+    .period = max(2, .period)
+  )$minimum
+  c(lambda_guerrero = lambda)
+}
