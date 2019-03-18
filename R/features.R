7dc0e502d93255f6033a3bf966fd5f7420ce3b78
Mitchell <mitch.ohara-wild@monash.edu>
2019-03-18 15:33:25 +1100

    Fixed generic consistency for features

--- R/features.R
+++ R/features.R
@@ -48,7 +48,7 @@ build_feature_calls <- function(measures, available_args){
 }
 
 #' @export
-features.tbl_ts <- function(data, y = NULL, ..., features = list(guerrero)){
+features.tbl_ts <- function(.data, y = NULL, ..., features = list(guerrero)){
   dots <- dots_list(...)
 
   if(is_function(features)){
@@ -58,9 +58,9 @@ features.tbl_ts <- function(data, y = NULL, ..., features = list(guerrero)){
   if(quo_is_null(enquo(y))){
     inform(sprintf(
       "Feature variable not specified, automatically selected `y = %s`",
-      measured_vars(data)[1]
+      measured_vars(.data)[1]
     ))
-    y <- sym(measured_vars(data)[1])
+    y <- sym(measured_vars(.data)[1])
   }
   else{
     y <- enexpr(y)
@@ -69,21 +69,55 @@ features.tbl_ts <- function(data, y = NULL, ..., features = list(guerrero)){
   features <- squash(features)
 
   if(is.null(dots$.period)){
-    dots$.period <- get_frequencies(NULL, data, .auto = "smallest")
+    dots$.period <- get_frequencies(NULL, .data, .auto = "smallest")
   }
 
-  data <- transmute(data, x = !!y)
+  .data <- transmute(.data, x = !!y)
 
   fns <- build_feature_calls(features, c(names(dots), "x"))
 
   with(dots,
-       data %>%
+       .data %>%
          as_tibble %>%
-         group_by(!!!key(data), !!!groups(data)) %>%
+         group_by(!!!key(.data), !!!groups(.data)) %>%
          summarise(
            .features = list(combine_features(!!!compact(fns)))
          ) %>%
          unnest(.features) %>%
          ungroup()
   )
+}
+
+#' @inherit tsfeatures::crossing_points
+#' @importFrom stats median
+#' @export
+crossing_points <- function(x)
+{
+  midline <- median(x, na.rm = TRUE)
+  ab <- x <= midline
+  lenx <- length(x)
+  p1 <- ab[1:(lenx - 1)]
+  p2 <- ab[2:lenx]
+  cross <- (p1 & !p2) | (p2 & !p1)
+  c(crossing_points = sum(cross, na.rm = TRUE))
+}
+
+#' @inherit tsfeatures::arch_stat
+#' @importFrom stats lm embed
+#' @export
+arch_stat <- function(x, lags = 12, demean = TRUE)
+{
+  if (length(x) <= 13) {
+    return(c(arch_lm = NA_real_))
+  }
+  if (demean) {
+    x <- x - mean(x, na.rm = TRUE)
+  }
+  mat <- embed(x^2, lags + 1)
+  fit <- try(lm(mat[, 1] ~ mat[, -1]), silent = TRUE)
+  if ("try-error" %in% class(fit)) {
+    return(c(arch_lm = NA_real_))
+  }
+  arch.lm <- summary(fit)
+  c(arch_lm = arch.lm$r.squared)
 }
