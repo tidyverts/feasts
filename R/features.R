40605395481205b08542ffef7bf4596b0716e97e
Mitchell <mitch.ohara-wild@monash.edu>
2019-04-24 13:52:29 +1000

    Improvements to features()

--- R/features.R
+++ R/features.R
@@ -1,10 +1,10 @@
 #' Extract features from a dataset
 #'
-#' @param .data A dataset (tsibble)
-#' @param ... Additional arguments to be passed to measures that use it.
+#' @param .tbl A dataset (tsibble/tibble)
+#' @param ... Additional arguments to be passed to measures.
 #'
 #' @export
-features <- function(.data, ...){
+features <- function(.tbl, ...){
   UseMethod("features")
 }
 
@@ -14,36 +14,13 @@ tbl_features <- function(features){
   }
 }
 
-build_feature_calls <- function(measures, available_args){
-  # Build measure calls
-  missing_args <- chr()
-  fns <- map(measures, function(fn){
-    args <- formals(fn)
-    args <- args[names(args) != "..."]
-    req_args <- names(args)[map_lgl(args, is_missing)]
-    if(!all(match_req <- req_args %in% available_args)){
-      missing_args <<- c(missing_args, req_args[!match_req])
-      return(NULL)
-    }
-
-    # Function call
-    inputs <- available_args[available_args%in%names(args)]
-    call2(fn, !!!set_names(syms(inputs), inputs))
-  })
-
-  if(!is_empty(missing_args)){
-    warn(
-      sprintf("Could not estimate all measures as the following arguments are missing: %s",
-              paste0(missing_args, collapse = ", "))
-    )
-  }
-
-  # names(fns) <- names(fns) %||% seq_along(fns)
-  fns
-}
-
+#' @rdname features
+#'
+#' @inheritParams dplyr::summarise_at
+#' @param features A list of functions (or lambda expressions) for the features to compute.
+#'
 #' @export
-features.tbl_ts <- function(.tbl, .vars = NULL, features, ...){
+features.tbl_ts <- function(.tbl, .vars = NULL, features = list(), ...){
   dots <- dots_list(...)
 
   if(is_function(features)){
@@ -51,7 +28,7 @@ features.tbl_ts <- function(.tbl, .vars = NULL, features, ...){
   }
   features <- map(squash(features), rlang::as_function)
 
-  if(possibly(is.null, FALSE)(.vars)){
+  if(quo_is_null(enquo(.vars))){
     inform(sprintf(
       "Feature variable not specified, automatically selected `y = %s`",
       measured_vars(.tbl)[1]
@@ -66,17 +43,15 @@ features.tbl_ts <- function(.tbl, .vars = NULL, features, ...){
     dots$.period <- get_frequencies(NULL, .tbl, .auto = "smallest")
   }
 
-  fns <- build_feature_calls(features, c(names(dots), "x"))
-
   as_tibble(.tbl) %>%
-    group_by(!!!key(.tbl), !!!groups(.tbl)) %>%
-    summarise_at(
+    group_by(!!!key(.tbl), !!!dplyr::groups(.tbl)) %>%
+    dplyr::summarise_at(
       .vars = .vars,
       .funs = tbl_features(features),
       !!!dots
     ) %>%
     unnest(!!!.vars, .sep = "_") %>%
-    ungroup()
+    dplyr::ungroup()
 }
 
 #' @inherit tsfeatures::crossing_points
