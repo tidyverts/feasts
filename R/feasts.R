7143f842c6523a6ce21a34d0cb59eefed341fad6
Mitchell <mitch.ohara-wild@monash.edu>
2019-05-03 18:09:37 +1000

    Fixed missing dplyr::ungroup import

--- R/feasts.R
+++ R/feasts.R
@@ -5,7 +5,7 @@ globalVariables(".")
 
 #' @import rlang
 #' @import tsibble
-#' @importFrom dplyr mutate transmute summarise group_by filter distinct lead
+#' @importFrom dplyr mutate transmute summarise group_by ungroup filter distinct lead
 #' @importFrom tidyr nest unnest gather
 NULL
 
