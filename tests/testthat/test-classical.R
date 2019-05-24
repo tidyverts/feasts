497c76fd5c77d9c4e6d493b5e732699f2d65cd48
Mitchell <mitch.ohara-wild@monash.edu>
2019-05-24 10:57:31 +1000

    Added classical decomposition tests
    Related: #48

---
+++ tests/testthat/test-classical.R
@@ -0,0 +1,47 @@
+context("test-classical")
+
+test_that("Additive classical decomposition", {
+  tsbl_co2 <- as_tsibble(co2)
+  dcmp <- tsbl_co2 %>% classical_decomposition(value)
+  stats_dcmp <- stats::decompose(co2)
+
+  expect_equivalent(
+    dcmp$trend,
+    unclass(stats_dcmp$trend)
+  )
+  expect_equivalent(
+    dcmp$seasonal,
+    unclass(stats_dcmp$seasonal)
+  )
+  expect_equivalent(
+    dcmp$random,
+    unclass(stats_dcmp$random)
+  )
+  expect_equal(
+    dcmp$value - dcmp$seasonal,
+    dcmp$seas_adjust
+  )
+})
+
+test_that("Multiplicative classical decomposition", {
+  tsbl_uad <- as_tsibble(USAccDeaths)
+  dcmp <- tsbl_uad %>% classical_decomposition(value, type = "multiplicative")
+  stats_dcmp <- stats::decompose(USAccDeaths, type = "multiplicative")
+
+  expect_equivalent(
+    dcmp$trend,
+    unclass(stats_dcmp$trend)
+  )
+  expect_equivalent(
+    dcmp$seasonal,
+    unclass(stats_dcmp$seasonal)
+  )
+  expect_equivalent(
+    dcmp$random,
+    unclass(stats_dcmp$random)
+  )
+  expect_equal(
+    dcmp$value / dcmp$seasonal,
+    dcmp$seas_adjust
+  )
+})
