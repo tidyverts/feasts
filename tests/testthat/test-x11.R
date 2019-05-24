650029c9ebe944bfe5d13d7cd10b78b818fb18f6
Mitchell <mitch.ohara-wild@monash.edu>
2019-05-24 11:44:52 +1000

    Added X11 decomposition tests
    Related: #48

---
+++ tests/testthat/test-x11.R
@@ -0,0 +1,65 @@
+context("test-x11")
+
+test_that("Bad inputs for X11 decomposition", {
+  expect_error(
+    tsibble::pedestrian %>%
+      filter(Sensor == "Southern Cross Station",
+             Date == as.Date("2015-01-01")) %>%
+      X11(Count),
+    "The X11 method only supports monthly"
+  )
+
+  expect_error(
+    as_tsibble(co2) %>%
+      X11(value ~ seq_along(value)),
+    "Exogenous regressors are not supported for X11 decompositions"
+  )
+})
+
+test_that("Additive X11 decomposition", {
+  tsbl_co2 <- as_tsibble(co2)
+  dcmp <- tsbl_co2 %>% X11(value)
+  seas_dcmp <- seasonal::seas(co2, x11="", x11.mode = "add",
+                              transform.function = "none")
+
+  expect_equivalent(
+    dcmp$trend,
+    unclass(seas_dcmp$data[,"trend"])
+  )
+  expect_equivalent(
+    dcmp$seasonal,
+    unclass(seas_dcmp$data[,"adjustfac"])
+  )
+  expect_equivalent(
+    dcmp$irregular,
+    unclass(seas_dcmp$data[,"irregular"])
+  )
+  expect_equal(
+    dcmp$value - dcmp$seasonal,
+    dcmp$seas_adjust
+  )
+})
+
+test_that("Multiplicative X11 decomposition", {
+  tsbl_uad <- as_tsibble(USAccDeaths)
+  dcmp <- tsbl_uad %>% X11(value, type = "multiplicative")
+  seas_dcmp <- seasonal::seas(USAccDeaths, x11="", x11.mode = "mult",
+                              transform.function = "log")
+
+  expect_equivalent(
+    dcmp$trend,
+    unclass(seas_dcmp$data[,"trend"])
+  )
+  expect_equivalent(
+    dcmp$seasonal,
+    unclass(seas_dcmp$data[,"adjustfac"])
+  )
+  expect_equivalent(
+    dcmp$irregular,
+    unclass(seas_dcmp$data[,"irregular"])
+  )
+  expect_equal(
+    dcmp$value / dcmp$seasonal,
+    dcmp$seas_adjust
+  )
+})
