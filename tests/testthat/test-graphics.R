89a5e57f9046620813acfb496248a2e223a820a0
Mitchell <mitch.ohara-wild@monash.edu>
2019-05-27 12:19:25 +1000

    Added gg_subseries tests

--- tests/testthat/test-graphics.R
+++ tests/testthat/test-graphics.R
@@ -4,6 +4,7 @@ tsbl_co2 <- as_tsibble(co2)
 tsbl_ped <- tsibble::pedestrian %>%
   filter(Sensor == "Southern Cross Station",
          yearmonth(Date) == yearmonth("2015 Jan"))
+
 test_that("gg_season() plots", {
   p <- gg_season(tsbl_co2, value)
 
@@ -90,3 +91,42 @@ test_that("gg_season() facets", {
     list(x = "index", y = "value")
   )
 })
+
+test_that("gg_subseries() plots", {
+  p <- gg_subseries(tsbl_co2, value)
+
+  expect_equal(
+    ggplot2::layer_data(p)$y,
+    tsbl_co2$value[order((seq_along(tsbl_co2$value) - 1)%%12)]
+  )
+  expect_equivalent(
+    as.numeric(table(ggplot2::layer_data(p)$PANEL)),
+    rep(39, 12)
+  )
+
+  p_built <- ggplot2::ggplot_build(p)
+
+  expect_identical(
+    p_built$plot$labels[c("x", "y")],
+    list(x = "index", y = "value")
+  )
+
+  p <- gg_subseries(tsbl_ped, Count, "day")
+
+  expect_equal(
+    ggplot2::layer_data(p)$y,
+    tsbl_ped$Count[order((seq_along(tsbl_ped$Count) - 1)%%24)]
+  )
+  expect_equivalent(
+    as.numeric(table(ggplot2::layer_data(p)$PANEL)),
+    rep(31, 24)
+  )
+
+  p_built <- ggplot2::ggplot_build(p)
+
+  expect_identical(
+    p_built$plot$labels[c("x", "y")],
+    list(x = "Date_Time", y = "Count")
+  )
+})
+
