10b928c99eed346a02d245417bf82c795a6090fe
Mitchell <mitch.ohara-wild@monash.edu>
2019-06-12 17:04:38 +1000

    Revert "Removed unused tidyr imports"
    This reverts commit 847d165f10fdc324098ae3fbe78c08e32a0dd6dc.

--- R/feasts.R
+++ R/feasts.R
@@ -6,6 +6,6 @@ globalVariables(".")
 #' @import rlang
 #' @import tsibble
 #' @importFrom dplyr mutate transmute summarise group_by ungroup filter distinct lead
-#' @importFrom tidyr gather
+#' @importFrom tidyr nest unnest gather
 NULL
 
