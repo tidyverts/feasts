6ee1bbc31a289582cc100942767079abe056f6a7
Mitchell <mitch.ohara-wild@monash.edu>
2019-08-26 20:01:22 +1000

    Use dev pkgdown

--- tic.R
+++ tic.R
@@ -11,12 +11,13 @@ if (Sys.getenv("BUILD_PKGDOWN") != "" && ci()$get_branch() == "master") {
     add_step(step_setup_push_deploy(path = "docs", branch = "gh-pages"))
 
   get_stage("deploy") %>%
+    add_step(step_install_github(c("r-lib/pkgdown"))) %>%
     add_code_step(
       pkgbuild::compile_dll(),
       prepare_call = remotes::install_github("r-lib/pkgbuild")
     ) %>%
     add_code_step(
-      pkgdown::build_favicon(),
+      pkgdown::build_favicons(),
       prepare_call = install.packages("magick")
     ) %>%
     add_step(step_build_pkgdown(run_dont_run = TRUE)) %>%
