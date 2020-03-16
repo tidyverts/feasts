6c249a88d93c51d33b39e725b39e0ec3d7bd4ca3
Mitchell <mail@mitchelloharawild.com>
2020-03-17 09:35:34 +1100

    Import utils::packageVersion

--- R/deprecated.R
+++ R/deprecated.R
@@ -2,9 +2,9 @@
 # They are due to be removed when these packages are released.
 
 default_time_units <- function(...){
-  if(packageVersion("tsibble") >= "0.8.9.9000") tsibble::default_time_units(...) else tsibble::time_unit(...)
+  if(utils::packageVersion("tsibble") >= "0.8.9.9000") tsibble::default_time_units(...) else tsibble::time_unit(...)
 }
 
 units_since <- function(...){
-  if(packageVersion("tsibble") >= "0.8.9.9000") as.double(...) else tsibble::units_since(...)
+  if(utils::packageVersion("tsibble") >= "0.8.9.9000") as.double(...) else tsibble::units_since(...)
 }
